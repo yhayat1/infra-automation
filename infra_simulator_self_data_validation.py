3e8e97b412dfb4c341ab5ea0eb4800b422bda7c1
Yossi Hayat <yossihayat4012@gmail.com>
2025-08-02 15:45:07 +0300

    Unneccessary remarked lines removed

--- infra_simulator_self_data_validation.py
+++ infra_simulator_self_data_validation.py
@@ -19,24 +19,6 @@ def define_path(dir, file):
 json_config_file = define_path("configs", "instances.json")
 logs_file = define_path("logs", "provisioning.log")
 bash_script_file = define_path("scripts", "install_services.sh")
-# print(json_config_file)
-# print(logs_file)
-# print(bash_script_file)
-
-
-# instances.json config file path config
-#configs_dir = os.path.join(base_dir, "configs")
-#json_config_file = os.path.join(configs_dir, "instances.json")
-# provisioning.log file path config
-#logs_dir = os.path.join(base_dir, "logs")
-#logs_file = os.path.join(logs_dir, "provisioning.log")
-# install_services.sh file path config
-#script_dir = os.path.join(base_dir, "scripts")
-#bash_script_file = os.path.join(script_dir, "install_services.sh")
-
-# Create log and config dirs if not exists
-#os.makedirs(configs_dir, exist_ok=True)
-#os.makedirs(logs_dir, exist_ok=True)
    
 # Define logging configuration
 logging.basicConfig(
